ae4f3337f41521db92914bc0db147b2295d9cad9
chansooda <munchamsu3263@naver.com>
2024-08-01 10:06:56 +0900

    Feat:흡연구역 추가 하기 addArea모듈 css추가

--- src/components/addComponent.tsx
+++ src/components/addComponent.tsx
@@ -1,30 +1,57 @@
-import React from "react";
+import React from 'react';
+import styles from "../../styles/addArea.module.css";
 
 export default function AddComponent() {
-return (
-    <div>
-        <h1>
-            <span style={{color: "red"}}>흡연구역</span>{' '}
-            <span style={{color: "black"}}>추가하기</span>
-        </h1>
-        <div>
-            <div>
-                <p>{'장소(주소)'}</p>
-                <button onClick={() => {}}>장소지정</button>
+    return (
+        <div className={styles.addArea}>
+            <p className={styles.title}>
+                <span className={styles.highlight}>흡연구역</span> 추가하기
+            </p>
+            <div className={styles.locationSection}>
+                <div className={styles.locationRow}>
+                    <div className={styles.locationLabelInput}>
+                        <p className={styles.sctitle}>장소(주소)</p>
+                        <input type="text" placeholder="주소를 입력하세요." className={styles.addressInput} />
+                    </div>
+                    <button className={`${styles.buttonCommon} ${styles.locationButton}`}>장소 지정</button>
+                </div>
+                <div className={styles.locationRow}>
+                    <div className={styles.locationLabelInput}>
+                        <p className={styles.sctitle}>상세장소</p>
+                        <input type="text" placeholder="상세 주소를 입력하세요." className={styles.addressInput} />
+                    </div>
+                    <button className={`${styles.buttonCommon} ${styles.areaButton}`}>영역 지정</button>
+                </div>
             </div>
-            <input type="text" className="input-text"/>
-        </div>
-        <div>
-            <div>
-                <p>{'상세주소'}</p>
-                <button onClick={() => {}}>영역지정</button>
+            <div className={styles.divisionSection}>
+                <p className={styles.sctitle}>흡연구역 구분</p>
+                <div className={styles.radioGroup}>
+                    <label>
+                        <input type="radio" name="smokingArea" value="open" />
+                        개방형
+                    </label>
+                    <label>
+                        <input type="radio" name="smokingArea" value="closed" />
+                        폐쇠형
+                    </label>
+                    <label>
+                        <input type="radio" name="smokingArea" value="line" />
+                        라인형
+                    </label>
+                </div>
+                <p className={styles.sctitle}>암묵적인 흡연구역 여부</p>
+                <div className={styles.radioGroup}>
+                    <label>
+                        <input type="radio" name="implicitSmokingArea" value="yes" />
+                        맞다
+                    </label>
+                    <label>
+                        <input type="radio" name="implicitSmokingArea" value="no" />
+                        아니다
+                    </label>
+                </div>
             </div>
-            <input type="text"/>
+            <button className={styles.addButton}>제출</button>
         </div>
-        <div>
-            <p>흡연구역 구분</p>
-            <input type="checkbox" style={{}}/>
-        </div>
-    </div>
-);
+    );
 }
