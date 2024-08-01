f47d7efc50e20f53971b652d861ce981168a1f59
choisiheon <0328ks@gmail.com>
2024-08-01 15:23:40 +0900

    Feat & Test: 흡연구역 검증기 DB 데이터 리스트로 불러오기 기능

--- src/components/settingArea.tsx
+++ src/components/settingArea.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import styles from "../../styles/settingArea.module.css";
-import verificationListStyles from "../../styles/verificationList.module.css"; // 리스트 스타일
 import InfoModal from "@/components/infoModal";
 import RedPillModal from "@/components/redpillModal";
 import WarningModal from "@/components/warningModal";
-import { testData } from "@/components/testData";
+import VerificationList from "@/components/verificationList";
 
 export default function SettingArea() {
     const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
     const [isRedPillModalOpen, setIsRedPillModalOpen] = useState(false);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+    const [isVerificationListOpen, setIsVerificationListOpen] = useState(false);
 
     const openInfoModal = () => setIsInfoModalOpen(true);
     const closeInfoModal = () => setIsInfoModalOpen(false);
@@ -21,23 +20,30 @@ export default function SettingArea() {
     const openWarningModal = () => setIsWarningModalOpen(true);
     const closeWarningModal = () => setIsWarningModalOpen(false);
 
-    const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+    const openVerificationList = () => setIsVerificationListOpen(true);
+    const closeVerificationList = () => setIsVerificationListOpen(false);
 
     return (
         <div className={styles.settingArea}>
             <button className={styles.settingButton} onClick={openInfoModal}>
                 기타 금연구역 안내
             </button>
-            <button className={styles.settingButton} onClick={openWarningModal}> {/* 변경 */}
+            <button className={styles.settingButton} onClick={openVerificationList}>
                 흡연구역 검증기
             </button>
-            <button className={styles.settingButton} onClick={openRedPillModal}> {/* 변경 */}
+            <button className={styles.settingButton} onClick={openRedPillModal}>
                 빨간약
             </button>
 
             <InfoModal isOpen={isInfoModalOpen} onClose={closeInfoModal} />
-            <WarningModal isOpen={isWarningModalOpen} onClose={closeWarningModal} /> {/* 추가 */}
+            <WarningModal isOpen={isWarningModalOpen} onClose={closeWarningModal} />
             <RedPillModal isOpen={isRedPillModalOpen} onClose={closeRedPillModal} />
+
+            {isVerificationListOpen && (
+                <div className={styles.verificationListWrapper}>
+                    <VerificationList onClose={closeVerificationList} />
+                </div>
+            )}
         </div>
     );
 }
