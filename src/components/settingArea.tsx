2ec2ef29501f34d8ae050d9a4b08af790e46bb4a
choisiheon <0328ks@gmail.com>
2024-08-11 13:17:32 +0900

    Design : 사이드바, 설정 컴포넌트 모바일 디자인 적용 완료

--- src/components/settingArea.tsx
+++ src/components/settingArea.tsx
@@ -3,14 +3,17 @@ import styles from "../../styles/settingArea.module.css";
 import InfoModal from "@/components/infoModal";
 import RedPillModal from "@/components/redpillModal";
 import WarningModal from "@/components/warningModal";
-import VerificationList from "@/components/verificationList";
 import FeedbackModal from '@/components/feedbackModal';
+import LOGO_ICON from "../../public/logo.svg";
 
-export default function SettingArea() {
+interface SettingAreaProps {
+    onClose: () => void;
+}
+
+export default function SettingArea({ onClose }: SettingAreaProps) {
     const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
     const [isRedPillModalOpen, setIsRedPillModalOpen] = useState(false);
     const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
-    const [isVerificationListOpen, setIsVerificationListOpen] = useState(false);
     const [isFeedbackModalOpen, setIsFeedbackModalOpen] = useState(false);
 
     const openInfoModal = () => setIsInfoModalOpen(true);
@@ -22,37 +25,39 @@ export default function SettingArea() {
     const openWarningModal = () => setIsWarningModalOpen(true);
     const closeWarningModal = () => setIsWarningModalOpen(false);
 
-    const openVerificationList = () => setIsVerificationListOpen(true);
-    const closeVerificationList = () => setIsVerificationListOpen(false);
-
-    const openFeedbackModal = () => {setIsFeedbackModalOpen(true);};
-    const closeFeedbackModal = () => {setIsFeedbackModalOpen(false);};
+    const openFeedbackModal = () => { setIsFeedbackModalOpen(true); };
+    const closeFeedbackModal = () => { setIsFeedbackModalOpen(false); };
 
     return (
         <div className={styles.settingArea}>
-            <button className={styles.settingButton} onClick={openInfoModal}>
-                기타 금연구역 안내
-            </button>
-            <button className={styles.settingButton} onClick={openWarningModal}>
-                흡연구역 검증기
-            </button>
-            <button className={styles.settingButton} onClick={openRedPillModal}>
-                빨간약
-            </button>
-            <button className={styles.settingButton} onClick={openFeedbackModal}>
-                피드백하기
-            </button>
 
-            <InfoModal isOpen={isInfoModalOpen} onClose={closeInfoModal} />
-            <WarningModal isOpen={isWarningModalOpen} onClose={closeWarningModal} />
-            <RedPillModal isOpen={isRedPillModalOpen} onClose={closeRedPillModal} />
-            <FeedbackModal isOpen={isFeedbackModalOpen} onClose={closeFeedbackModal} />
+            <button className={styles.closeButton} onClick={onClose}>&times;</button>
 
-            {/*{isVerificationListOpen && (*/}
-            {/*    <div className={styles.verificationListWrapper}>*/}
-            {/*        <VerificationList onClose={closeVerificationList} />*/}
-            {/*    </div>*/}
-            {/*)}*/}
+            {/* 모바일에서만 보이는 헤더 */}
+            <div className={styles.modalHeader}>
+                <LOGO_ICON className={styles.logoIcon}/>
+                <h1 className={styles.cleanBreath}>CleanBreath</h1>
+            </div>
+
+            <div className={styles.mobileMargin}>
+                <button className={styles.settingButton} onClick={openInfoModal}>
+                    기타 금연구역 안내
+                </button>
+                <button className={styles.settingButton} onClick={openWarningModal}>
+                    흡연구역 검증기
+                </button>
+                <button className={styles.settingButton} onClick={openRedPillModal}>
+                    빨간약
+                </button>
+                <button className={styles.settingButton} onClick={openFeedbackModal}>
+                    피드백하기
+                </button>
+
+                <InfoModal isOpen={isInfoModalOpen} onClose={closeInfoModal}/>
+                <WarningModal isOpen={isWarningModalOpen} onClose={closeWarningModal}/>
+                <RedPillModal isOpen={isRedPillModalOpen} onClose={closeRedPillModal}/>
+                <FeedbackModal isOpen={isFeedbackModalOpen} onClose={closeFeedbackModal}/>
+            </div>
         </div>
     );
 }
