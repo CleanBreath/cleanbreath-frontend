3bd08ed12f276fd718198df4dc379942e1d57b09
CMU02 <Hyeonjun1968@naver.com>
2024-08-14 16:55:37 +0900

    - FEAT : 흡연구역 추가 요청 기능 구현 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ import ReactGA from 'react-ga4';
 import SMOK_ICON from "../../public/smokMarker.png";
 import NONSMOK_ICON from "../../public/nonSmokMarker.png"
 import Image from "next/image";
+import { Address } from '@/interface/AddressInterface';
+import DrawingField from '@/components/drawingField';
+import { get } from 'http';
 
 const APP_KEY = '6cf24fc76a6d5ae29260b2a99b27b49a';
 const TRACKING_ID = "G-YPYE7W46DT";
@@ -38,6 +41,55 @@ export default function Home() {
   const [PolygonState, setPolygonState] = useState<string | null>(null);
   const [statute, setStatute] = useState<string | null>(null);
   const [zoomalbe, setZoomable] = useState(true);
+  
+  const [address, setAddress] = useState<Address[]>([]); // 콜백 패턴으로 받아온 주소 데이터들 state 변수
+  const [activeFunc, setActiveFunc] = useState<string | null>(null); // Add Component 활성화 함수 state 변수
+  const [position, setPosition] = useState<{ lat: number; lng: number }>({lat: 0, lng: 0}); // 마커 위치 state 변수
+  const [mousePosition, setMousePosition] = useState({lat: 0, lng: 0}); // 마우스 위치 state 변수
+  const [path, setPath] = useState<{lat : number, lng : number}[]>([]) // 영역 값 state 변수
+  const [polygon, setPolygon] = useState<kakao.maps.Polygon>(); // Polygon 객체 state 변수
+  const [isDrawing, setIsDrawing] = useState(false); // 영역 그리기 상태 변수
+
+  // 위도 및 경도 값을 받아서 주소를 반환하는 함수
+  const getPositionToAddress = (_map : kakao.maps.Map, mouseEvent : kakao.maps.event.MouseEvent) => {
+    const latLng = mouseEvent.latLng;
+    const geocoder = new kakao.maps.services.Geocoder();
+    setPosition({lat: latLng.getLat(), lng: latLng.getLng()});
+    geocoder.coord2Address(latLng.getLng(), latLng.getLat(), getPositionCallback);
+  }
+
+  // getPositionToAddress callback 함수
+  const getPositionCallback = (result : any, status : string) => {
+    if(status === kakao.maps.services.Status.OK) {
+      setAddress(result);
+    }
+  }
+
+  // AddComponent 영역 그리기 시작 클릭 이벤트
+  const handleAddressPositionStartClick = (_map : kakao.maps.Map, mouseEvent : kakao.maps.event.MouseEvent) => {
+    if(!isDrawing) {
+      setPath([]);
+    }
+    setPath((prev) => [...prev, {lat: mouseEvent.latLng.getLat(), lng: mouseEvent.latLng.getLng()}]);
+    setIsDrawing(true);
+  }
+  // AddComponent 영역 그리기 종료 클릭 이벤트
+  const handleAddressPositionEndClick = (_map : kakao.maps.Map, mouseEvent : kakao.maps.event.MouseEvent) => {
+    setActiveFunc(null);
+    setIsDrawing(false);
+  }
+
+  // AddComponent 마우스 이동 이벤트
+  const handleMouseMove = (_map : kakao.maps.Map, mouseEvent : kakao.maps.event.MouseEvent) => {
+    setMousePosition({lat: mouseEvent.latLng.getLat(), lng: mouseEvent.latLng.getLng()});
+  }
+
+  // AddComponent 함수 활성화 토글 함수
+  const togleAddFunc = (funcName : string | null) => {
+    setActiveFunc(activeFunc === funcName ? null : funcName);
+  }
+
+
 
   useEffect(() => {
     ReactGA.initialize(TRACKING_ID);
@@ -111,9 +163,8 @@ export default function Home() {
   if (error) {
     return <div>Error loading Kakao Map: {error.message}</div>;
   }
-
   return (
-    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
       <SideMenu
         onListClick={handleListClick}
         activeMenu={activeMenu}
@@ -121,8 +172,12 @@ export default function Home() {
         isData={isData}
         isLoading={isLoading}
         error={errorMsg}
+        toggleAddFunc={togleAddFunc}
+        addressData={address}
+        position={position}
+        path={path}
       />
-      <AreaToggleComponent 
+      <AreaToggleComponent
         isNonSmoking={isNonSmoking}
         isSmoking={isSmoking}
         nonSmokingToggle={nonSmokingToggle}
@@ -131,11 +186,29 @@ export default function Home() {
       <Map
         center={center}
         isPanto={true}
-        style={{ width: '100%', height: '100%', zIndex: 0 }}
+        style={{ width: "100%", height: "100%", zIndex: 0 }}
         minLevel={6}
         level={3}
         zoomable={zoomalbe}
+        onClick={(_, mouseEvent) => {
+          if(activeFunc === "getAddress") {
+            getPositionToAddress(_, mouseEvent);
+          }
+          if(activeFunc === "drawPolygonStart") {
+            handleAddressPositionStartClick(_, mouseEvent);
+          }
+        }}
+        onDoubleClick={handleAddressPositionEndClick}
+        onMouseMove={handleMouseMove}
       >
+        <MapMarker position={position ?? center} />
+        <DrawingField
+            activeFunc={activeFunc}
+            setPolygon={setPolygon}
+            path={path}
+            mousePosition={mousePosition}
+        />
+
         {isOverlayClicked && markerPosition && (
           <MarkerOverlay
             markerPosition={markerPosition}
@@ -148,36 +221,42 @@ export default function Home() {
           />
         )}
 
-        {userLocation && (
-          <MapMarker position={userLocation}>
-          </MapMarker>
-        )}
+        {userLocation && <MapMarker position={userLocation}></MapMarker>}
         {isSmoking && isData.length > 0 && (
-            <MarkerClusterer averageCenter={true} minLevel={3}>
-              {isData.flatMap((item) =>
-                item.paths
-                  .filter(path => path.divisionArea.startsWith('SMOKING_ZONE'))
-                  .map((pathIndex) => (
-                    <CustomOverlayMap
-                      key={`${item.address_idx}-${pathIndex.divisionArea}`}
-                      position={{
-                        lat: item.address_latitude,
-                        lng: item.address_longitude
-                      }}
-                      yAnchor={1}
-                    >
-                      <Image src={SMOK_ICON} alt={"Smok"} width={40} height={60} onClick={() => {
+          <MarkerClusterer averageCenter={true} minLevel={3}>
+            {isData.flatMap((item) =>
+              item.paths
+                .filter((path) => path.divisionArea.startsWith("SMOKING_ZONE"))
+                .map((pathIndex) => (
+                  <CustomOverlayMap
+                    key={`${item.address_idx}-${pathIndex.divisionArea}`}
+                    position={{
+                      lat: item.address_latitude,
+                      lng: item.address_longitude,
+                    }}
+                    yAnchor={1}
+                  >
+                    <Image
+                      src={SMOK_ICON}
+                      alt={"Smok"}
+                      width={40}
+                      height={60}
+                      onClick={() => {
                         setIsOverlayClicked(true);
-                        handlePolygonClick(item.address_latitude, item.address_longitude);
-                      }}/>
-                    </CustomOverlayMap>
-                  ))
-              )}
-            </MarkerClusterer>
+                        handlePolygonClick(
+                          item.address_latitude,
+                          item.address_longitude
+                        );
+                      }}
+                    />
+                  </CustomOverlayMap>
+                ))
+            )}
+          </MarkerClusterer>
         )}
 
         {isData.length > 0 && (
-          <Polygon 
+          <Polygon
             isData={isData}
             isApartmentsData={isApartmentsData}
             isNonSmoking={isNonSmoking}
